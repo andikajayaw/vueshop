e18f6eb2714113b8d0e218881aa824cc1a44eea4
unknown <andikajayawiguna@gmail.com>
2022-06-20 23:06:26 +0800

    create Categories All View component

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/CategoriesView.vue')
+  },
 ]
 
 const router = new VueRouter({
